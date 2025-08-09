ef221c66ddc1d50a6bbf58314a97436fe1a57753
Tomsooer <tsoer2013@gmail.com>
2025-08-09 20:56:44 +0300

    add some images and fixed stylles for naw

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 const menuIcon = document.querySelector('.menu-icon');
 const header = document.querySelector('header');
+const headerNav = document.querySelector('nav');
 const body = document.querySelector('body');
-
-console.log("menuIcon: ", menuIcon);
-console.log("header: ", header);
-console.log("body: ", body);
 
 menuIcon.addEventListener('click', () => {
 	menuIcon.classList.toggle('menu-icon--active');
 	header.classList.toggle('header--mobile');
+	headerNav.classList.toggle('header__nav-wrap');
 	body.classList.toggle('no-scroll');
 });
